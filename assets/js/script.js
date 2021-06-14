947f7f5d1f738be486857597e3f829debd6d9389
mwmlln <mwmlln@gmail.com>
2021-06-14 14:06:34 +0000

    ADD display random image for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "game-start") {
-                alert("You clicked start!");
+                startGame();
             } else {
                 let keytype = this.getAttribute("data-type");
                 alert(`You clicked ${keytype}`)
@@ -12,3 +15,38 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
+
+
+// function to start the process of the game
+
+function startGame() {
+    runTimer();
+    displayImg();
+}
+
+//Run countdown Timer for game duration of 60seconds
+
+function runTimer() {
+    
+}
+
+//  Display random selection of the images 
+
+function displayImg() {
+    let images = new Array()
+    images = [
+        '<img src="assets/images/note_c.png" alt="Music notation image" class="note-img">',
+        '<img src="assets/images/note_d.png" alt="Music notation image" class="note-img">',
+        '<img src="assets/images/note_e.png" alt="Music notation image" class="note-img">',
+        '<img src="assets/images/note_f.png" alt="Music notation image" class="note-img">',
+        '<img src="assets/images/note_g.png" alt="Music notation image" class="note-img">',
+        '<img src="assets/images/note_a.png" alt="Music notation image" class="note-img">',
+        '<img src="assets/images/note_b.png" alt="Music notation image" class="note-img">',
+    ];
+    let dispImg = images[Math.floor(Math.random() * images.length)];
+    document.getElementById("img-place").innerHTML = dispImg
+}
+
+function isCorrect() {
+
+}
