f08b8234692e3f37d6cf1e7a72c2d2f4ccf880ab
mwmlln <mwmlln@gmail.com>
2021-06-14 20:04:20 +0000

    ADD data-type to images

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "game-start") {
-                startGame();
-            } else {
-                let keytype = this.getAttribute("data-type");
-                alert(`You clicked ${keytype}`)
+                if (this.getAttribute("data-type") === "game-start") {
+                    startGame();
+                } else { 
+                    let currentImg = document.getElementById("img-place").innerHTML
+                    if (this.getAttribute("data-type") === currentImg("data-type")) {
+                        scoreUp()
+                    }
+                        
+                        let keytype = this.getAttribute("data-type");
+                    }
+  
+                } 
             }
         })
-    }
+}
 })
 
 
@@ -27,7 +34,7 @@ function startGame() {
 //Run countdown Timer for game duration of 60seconds
 
 function runTimer() {
-    
+
 }
 
 //  Display random selection of the images 
@@ -35,18 +42,28 @@ function runTimer() {
 function displayImg() {
     let images = new Array()
     images = [
-        '<img src="assets/images/note_c.png" alt="Music notation image" class="note-img">',
-        '<img src="assets/images/note_d.png" alt="Music notation image" class="note-img">',
-        '<img src="assets/images/note_e.png" alt="Music notation image" class="note-img">',
-        '<img src="assets/images/note_f.png" alt="Music notation image" class="note-img">',
-        '<img src="assets/images/note_g.png" alt="Music notation image" class="note-img">',
-        '<img src="assets/images/note_a.png" alt="Music notation image" class="note-img">',
-        '<img src="assets/images/note_b.png" alt="Music notation image" class="note-img">',
+        '<img src="assets/images/note_c.png" alt="Music notation image" data-type="c" class="note-img">',
+        '<img src="assets/images/note_d.png" alt="Music notation image" data-type="d" class="note-img">',
+        '<img src="assets/images/note_e.png" alt="Music notation image" data-type="e" class="note-img">',
+        '<img src="assets/images/note_f.png" alt="Music notation image" data-type="f" class="note-img">',
+        '<img src="assets/images/note_g.png" alt="Music notation image" data-type="g" class="note-img">',
+        '<img src="assets/images/note_a.png" alt="Music notation image" data-type="a" class="note-img">',
+        '<img src="assets/images/note_b.png" alt="Music notation image" data-type="b" class="note-img">',
     ];
     let dispImg = images[Math.floor(Math.random() * images.length)];
     document.getElementById("img-place").innerHTML = dispImg
 }
 
 function isCorrect() {
+    let currentImg = document.getElementById("img-place").innerHTML
+    let keytype = this.getAttribute("data-type");
+
+}
+
+function scoreUp(){
+
+}
+
+updateScore() {
 
 }
