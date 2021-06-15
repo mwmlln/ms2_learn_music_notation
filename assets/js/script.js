2a93c2c876f1b9b3b9597072d3ca8adb032d19d2
mwmlln <mwmlln@gmail.com>
2021-06-15 15:23:46 +0000

    ADD correct wrong area in html and js obtains data-type from current image for chaecking answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,68 +2,54 @@
 // Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
+    let buttons = document.getElementsByClassName('game-btn');
     for (let button of buttons) {
         button.addEventListener("click", function () {
-                if (this.getAttribute("data-type") === "game-start") {
-                    startGame();
-                } else { 
-                    let currentImg = document.getElementById("img-place").innerHTML
-                    if (this.getAttribute("data-type") === currentImg("data-type")) {
-                        scoreUp()
-                    }
-                        
-                        let keytype = this.getAttribute("data-type");
-                    }
-  
-                } 
+            if (this.getAttribute("data-type") === "game-start") {
+                startGame();
+            } else {
+                isCorrect()
+
             }
         })
-}
-})
-
+    }
+});
 
 
 // function to start the process of the game
 
 function startGame() {
-    runTimer();
+
     displayImg();
-}
+};
 
 //Run countdown Timer for game duration of 60seconds
 
-function runTimer() {
+function runTimer() {};
 
-}
+
+
+// Set the queastion images and answers
+let images = [
+    '<img src="assets/images/note_c.png"  data-type="c" alt="Music notation image" class="note-img">',
+    '<img src="assets/images/note_d.png"  data-type="d" alt="Music notation image" class="note-img">',
+    '<img src="assets/images/note_e.png"  data-type="e" alt="Music notation image" class="note-img">',
+    '<img src="assets/images/note_f.png"  data-type="f" alt="Music notation image" class="note-img">',
+    '<img src="assets/images/note_g.png"  data-type="g" alt="Music notation image" class="note-img">',
+    '<img src="assets/images/note_a.png"  data-type="a" alt="Music notation image" class="note-img">',
+    '<img src="assets/images/note_b.png"  data-type="b" alt="Music notation image" class="note-img">',
+];
 
 //  Display random selection of the images 
-
 function displayImg() {
-    let images = new Array()
-    images = [
-        '<img src="assets/images/note_c.png" alt="Music notation image" data-type="c" class="note-img">',
-        '<img src="assets/images/note_d.png" alt="Music notation image" data-type="d" class="note-img">',
-        '<img src="assets/images/note_e.png" alt="Music notation image" data-type="e" class="note-img">',
-        '<img src="assets/images/note_f.png" alt="Music notation image" data-type="f" class="note-img">',
-        '<img src="assets/images/note_g.png" alt="Music notation image" data-type="g" class="note-img">',
-        '<img src="assets/images/note_a.png" alt="Music notation image" data-type="a" class="note-img">',
-        '<img src="assets/images/note_b.png" alt="Music notation image" data-type="b" class="note-img">',
-    ];
     let dispImg = images[Math.floor(Math.random() * images.length)];
-    document.getElementById("img-place").innerHTML = dispImg
-}
+    document.getElementById("img-place").innerHTML = dispImg;
+};
 
 function isCorrect() {
-    let currentImg = document.getElementById("img-place").innerHTML
-    let keytype = this.getAttribute("data-type");
+    let currentImg = document.getElementById("img-place").children[0];
+    let keytype = currentImg.getAttribute("data-type");
 
-}
+};
 
-function scoreUp(){
-
-}
-
-updateScore() {
-
-}
+function scoreUp() {};
